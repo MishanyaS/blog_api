7b443b688a97a62a6b0b4966a2dd7083f323ed30
MishanyaS <shilinm1203@gmail.com>
2026-02-20 09:41:29 +0200

    Replacing the Config class with model_config

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,5 @@
 from pydantic_settings import BaseSettings
-from pydantic import Field
+from pydantic import Field, ConfigDict
 
 class Settings(BaseSettings):
     app_name: str = Field(alias="APP_NAME")
@@ -33,8 +33,6 @@ class Settings(BaseSettings):
     def redis_url(self) -> str:
         return f"redis://{self.redis_host}:{self.redis_port}"
     
-    class Config:
-        env_file = ".env"
-        extra = "ignore"
+    model_config = ConfigDict(env_file = ".env", extra = "ignore")
 
 settings = Settings()
